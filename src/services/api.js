bf26e9b03127f420f66b4015c83a9a4ca3f36457
Mao sokchea <mao_sokchea@pp.bbu.edu.kh>
2026-08-08 12:59:48 +0700

    fix: update cors configuration for vercel frontend

--- src/services/api.js
+++ src/services/api.js
@@ -14,7 +14,6 @@ const getBaseUrl = () => {
 
 const api = axios.create({
     baseURL: getBaseUrl(),
-    withCredentials: true,
     headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
