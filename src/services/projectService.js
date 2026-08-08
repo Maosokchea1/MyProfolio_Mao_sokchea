5910dbecec8909c96a4a7745a5b90ad9b06d2e8c
Mao sokchea <mao_sokchea@pp.bbu.edu.kh>
2026-08-08 11:50:43 +0700

    Add getProjectBySlug to projectService

--- src/services/projectService.js
+++ src/services/projectService.js
@@ -4,3 +4,8 @@ export const getProjects = async () => {
     const response = await api.get("/projects");
     return response.data;
 };
+
+export const getProjectBySlug = async (slug) => {
+    const response = await api.get(`/projects/${slug}`);
+    return response.data;
+};
